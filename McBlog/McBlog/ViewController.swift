fb7f7cba306eabff25d07a3ec995e0b71adfae27
Theshy <475993847@qq.com>
2016-04-14 23:45:20 +0800

    新建项目

---
+++ McBlog/McBlog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  McBlog
+//
+//  Created by Theshy on 16/4/14.
+//  Copyright © 2016年 郭月辉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
