0aff0d1bc3cc5de6ebf049ea6a0292b5a9b66be3
yuehuig <yuehuig@jumei.com>
2016-04-29 18:50:12 +0800

    新特性页面初始化

--- McBlog/McBlog/AppDelegate.swift
+++ McBlog/McBlog/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.backgroundColor = UIColor.whiteColor()
-        window?.rootViewController = WelcomeViewController()
+        window?.rootViewController = NewFeautureController()
         window?.makeKeyAndVisible()
         
         setUpAppearance()
